036c56ebc222a76168637674c6dd6752b8c6b3d2
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-15 12:39:34 +0530

    repository updated

--- src/main/java/com/selvam/dreamshops/repository/ImageRepository.java
+++ src/main/java/com/selvam/dreamshops/repository/ImageRepository.java
@@ -3,5 +3,8 @@ package com.selvam.dreamshops.repository;
 import com.selvam.dreamshops.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageRepository extends JpaRepository<Image,Long> {
+    List<Image> findByProductId(Long id);
 }
--- src/main/java/com/selvam/dreamshops/repository/ProductRepository.java
+++ src/main/java/com/selvam/dreamshops/repository/ProductRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product,Long> {
     List<Product> findByCategoryName(String category);
 
-    List<Product> findByBrandName(String brand);
+    List<Product> findByBrand(String brand);
 
     List<Product> findByCategoryNameAndBrand(String category, String brand);
 
