88143c3236585324c34c1b5d673c2a4ac319b64f
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-17 06:58:56 +0530

    Cart Item Service added

--- src/main/java/com/selvam/dreamshops/service/cart/ICartItemService.java
+++ src/main/java/com/selvam/dreamshops/service/cart/ICartItemService.java
@@ -1,2 +1,11 @@
-package com.selvam.dreamshops.service.cart;public interface ICartItemService {
+package com.selvam.dreamshops.service.cart;
+
+import com.selvam.dreamshops.model.CartItem;
+
+public interface ICartItemService {
+    void addItemToCart(Long cartId,Long productId, int quantity);
+    void removeItemFromCart(Long cartId,Long productId);
+    void updateItemQuantity(Long cartId,Long productId, int quantity);
+
+    CartItem getCartItem(Long cartId, Long productId);
 }
