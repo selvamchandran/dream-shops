d635e8145c5a7f429acbd29f7d0b8247cb082fb3
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-18 07:22:24 +0530

    order entity added

--- src/main/java/com/selvam/dreamshops/model/Order.java
+++ src/main/java/com/selvam/dreamshops/model/Order.java
@@ -1,2 +1,33 @@
-package com.selvam.dreamshops.model;public class Order {
+package com.selvam.dreamshops.model;
+
+import com.selvam.dreamshops.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate OrderDate;
+    private BigDecimal totalAmount;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<OrderItem> orderItems = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
