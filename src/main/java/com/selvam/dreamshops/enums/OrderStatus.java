2b2488badc40f4c0dbb7deb743dd35bdbd65d893
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-18 07:21:42 +0530

    order status added

--- src/main/java/com/selvam/dreamshops/enums/OrderStatus.java
+++ src/main/java/com/selvam/dreamshops/enums/OrderStatus.java
@@ -1,2 +1,9 @@
-package com.selvam.dreamshops.enums;public class OrderStatus {
+package com.selvam.dreamshops.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIEVERED,
+    CANCELLED
 }
