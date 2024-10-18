26cda13204b26bcc770f0122ccf951fa19901a13
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-18 07:23:37 +0530

    order repository added

--- src/main/java/com/selvam/dreamshops/repository/OrderRepository.java
+++ src/main/java/com/selvam/dreamshops/repository/OrderRepository.java
@@ -1,2 +1,10 @@
-package com.selvam.dreamshops.repository;public class OrderRepository {
+package com.selvam.dreamshops.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.selvam.dreamshops.model.Order;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+    List<Order> findByUserId(Long userId);
 }
