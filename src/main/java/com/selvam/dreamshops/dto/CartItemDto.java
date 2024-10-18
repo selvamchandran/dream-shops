ccdfb9ff040d142215f868386604d0b9fb0ae589
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-18 07:26:02 +0530

    Cart Item Dto added

--- src/main/java/com/selvam/dreamshops/dto/CartItemDto.java
+++ src/main/java/com/selvam/dreamshops/dto/CartItemDto.java
@@ -1,2 +1,14 @@
-package com.selvam.dreamshops.dto;public class CartItemDto {
+package com.selvam.dreamshops.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class CartItemDto {
+    private Long itemId;
+    private int quantity;
+    private BigDecimal unitPrice;
+    private ProductDto product;
 }
