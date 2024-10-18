11f0d70e5d4701001db5bfc1fea11bd4ee6bc521
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-18 07:29:55 +0530

    User Repository added

--- src/main/java/com/selvam/dreamshops/repository/UserRepository.java
+++ src/main/java/com/selvam/dreamshops/repository/UserRepository.java
@@ -1,2 +1,8 @@
-package com.selvam.dreamshops.repository;public interface UserRepository {
+package com.selvam.dreamshops.repository;
+
+import com.selvam.dreamshops.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    boolean existsByEmail(String email);
 }
