07571e18793be7a533307beeb0876b3548346ee9
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-18 07:29:37 +0530

    User model added

--- src/main/java/com/selvam/dreamshops/model/User.java
+++ src/main/java/com/selvam/dreamshops/model/User.java
@@ -1,2 +1,33 @@
-package com.selvam.dreamshops.model;public class User {
+package com.selvam.dreamshops.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+import java.util.List;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @NaturalId
+    private String email;
+    private String password;
+
+    @OneToOne(mappedBy = "user" , cascade = CascadeType.ALL, orphanRemoval = true)
+    private Cart cart;
+
+    @OneToMany(mappedBy = "user" , cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> order;
+
 }
