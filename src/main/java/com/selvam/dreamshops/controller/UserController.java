35d3d1a6fe261248dca2bdcc3fee419b8de65a2a
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-18 07:31:18 +0530

    User controller added

--- src/main/java/com/selvam/dreamshops/controller/UserController.java
+++ src/main/java/com/selvam/dreamshops/controller/UserController.java
@@ -1,64 +1,80 @@
 package com.selvam.dreamshops.controller;
 
 import com.selvam.dreamshops.dto.OrderDto;
+import com.selvam.dreamshops.dto.UserDto;
+import com.selvam.dreamshops.exceptions.AlreadyExistsException;
 import com.selvam.dreamshops.exceptions.ResourceNotFoundException;
-import com.selvam.dreamshops.model.Cart;
 import com.selvam.dreamshops.model.Order;
+import com.selvam.dreamshops.model.User;
+import com.selvam.dreamshops.request.CreateUserRequest;
+import com.selvam.dreamshops.request.UpdateUserRequest;
 import com.selvam.dreamshops.response.ApiResponse;
-import com.selvam.dreamshops.service.cart.ICartService;
 import com.selvam.dreamshops.service.order.IOrderService;
+import com.selvam.dreamshops.service.user.IUserService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("${api.prefix}/orders")
-public class OrderController {
-    private final IOrderService orderService;
-
-    @PostMapping("/order")
-    public ResponseEntity<ApiResponse> createOrder(Long userId)
+@RequestMapping("${api.prefix}/users")
+public class UserController {
+    private final IUserService userService;
+    @GetMapping("/{userId}/user")
+    public ResponseEntity<ApiResponse> getUserById(@PathVariable Long userId)
     {
         try {
-            Order order = orderService.placeOrder(userId);
-            return ResponseEntity.ok(new ApiResponse("Item Order Success!", order));
-        } catch (Exception e) {
-            return ResponseEntity.status(INTERNAL_SERVER_ERROR)
-                    .body(new ApiResponse("Error Occured!",e.getMessage()));
+            User user = userService.getUserById(userId);
+            UserDto userDto = userService.convertUserToDto(user);
+            return ResponseEntity.ok(new ApiResponse("Success!", userDto));
+        }  catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(),null));
         }
     }
 
-    @GetMapping("/{orderId}/order")
-    public ResponseEntity<ApiResponse> getOrderById(@PathVariable Long orderId)
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> createUser(@RequestBody CreateUserRequest request)
     {
         try {
-            OrderDto order = orderService.getOrder(orderId);
-            return ResponseEntity.ok(new ApiResponse("Item Order Success!", order));
+            User user = userService.createUser(request);
+            UserDto userDto = userService.convertUserToDto(user);
+            return ResponseEntity.ok(new ApiResponse("Create User Success!", userDto));
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(CONFLICT)
+                    .body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+
+    @PutMapping("/{userId}/update")
+    public ResponseEntity<ApiResponse> updateUser(@RequestBody UpdateUserRequest request,@PathVariable Long userId)
+    {
+        try {
+            User user = userService.updateUser(request,userId);
+            UserDto userDto = userService.convertUserToDto(user);
+            return ResponseEntity.ok(new ApiResponse("Update user Success!", userDto));
         } catch (ResourceNotFoundException e) {
             return ResponseEntity.status(NOT_FOUND)
-                    .body(new ApiResponse("Oops!",e.getMessage()));
+                    .body(new ApiResponse(e.getMessage(),null));
         }
 
     }
 
-    @GetMapping("/{orderId}/orders")
-    public ResponseEntity<ApiResponse> getUserOrders(@PathVariable Long userId)
+    @DeleteMapping("/{userId}/delete")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Long userId)
     {
         try {
-            List<OrderDto> order = orderService.getUserOrders(userId);
-            return ResponseEntity.ok(new ApiResponse("Item Order Success!", order));
+            userService.deleteUser(userId);
+            return ResponseEntity.ok(new ApiResponse("Delete User success!", null));
         } catch (ResourceNotFoundException e) {
             return ResponseEntity.status(NOT_FOUND)
-                    .body(new ApiResponse("Oops!",e.getMessage()));
+                    .body(new ApiResponse(e.getMessage(),null));
         }
 
     }
-
 }
