4589831fe536c9c77c087256622751939f758f68
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-18 07:30:40 +0530

    User update request added

--- src/main/java/com/selvam/dreamshops/request/UpdateUserRequest.java
+++ src/main/java/com/selvam/dreamshops/request/UpdateUserRequest.java
@@ -1,2 +1,11 @@
-package com.selvam.dreamshops.request;public class UpdateUserRequest {
+package com.selvam.dreamshops.request;
+
+import lombok.Data;
+
+@Data
+public class UpdateUserRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 }
