a7a90034f8531b87faca0a842a7e0809528939d9
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-18 07:23:08 +0530

    order item dto added

--- src/main/java/com/selvam/dreamshops/dto/OrderItemDto.java
+++ src/main/java/com/selvam/dreamshops/dto/OrderItemDto.java
@@ -1,2 +1,17 @@
-package com.selvam.dreamshops.dto;public class OrderItemDTO {
+package com.selvam.dreamshops.dto;
+
+import com.selvam.dreamshops.model.Order;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+import java.math.BigDecimal;
+@Data
+public class OrderItemDto {
+    private Long productId;
+    private String productName;
+    private int quantity;
+    private BigDecimal price;
+
+
 }
