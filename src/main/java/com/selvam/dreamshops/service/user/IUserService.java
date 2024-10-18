16148dc625b4069d70fe4d92ceb2d281db4ec99b
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-18 07:31:58 +0530

    User service added

--- src/main/java/com/selvam/dreamshops/service/user/IUserService.java
+++ src/main/java/com/selvam/dreamshops/service/user/IUserService.java
@@ -1,2 +1,15 @@
-package com.selvam.dreamshops.service.user;public class IUserService {
+package com.selvam.dreamshops.service.user;
+
+import com.selvam.dreamshops.dto.UserDto;
+import com.selvam.dreamshops.model.User;
+import com.selvam.dreamshops.request.CreateUserRequest;
+import com.selvam.dreamshops.request.UpdateUserRequest;
+
+public interface IUserService {
+    User getUserById(Long userId);
+    User createUser(CreateUserRequest request);
+    User updateUser(UpdateUserRequest request, Long userId);
+    void deleteUser(Long userId);
+
+    UserDto convertUserToDto(User user);
 }
--- src/main/java/com/selvam/dreamshops/service/user/UserService.java
+++ src/main/java/com/selvam/dreamshops/service/user/UserService.java
@@ -1,2 +1,61 @@
-package com.selvam.dreamshops.service.user;public class UserService {
+package com.selvam.dreamshops.service.user;
+
+import com.selvam.dreamshops.dto.UserDto;
+import com.selvam.dreamshops.exceptions.AlreadyExistsException;
+import com.selvam.dreamshops.exceptions.ResourceNotFoundException;
+import com.selvam.dreamshops.model.User;
+import com.selvam.dreamshops.repository.UserRepository;
+import com.selvam.dreamshops.request.CreateUserRequest;
+import com.selvam.dreamshops.request.UpdateUserRequest;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements IUserService {
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+    @Override
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User nor found!"));
+    }
+
+    @Override
+    public User createUser(CreateUserRequest request) {
+        return Optional.of(request)
+                .filter(user->!userRepository.existsByEmail(request.getEmail()))
+                .map(req->{
+                    User user = new User();
+                    user.setEmail(request.getEmail());
+                    user.setPassword(request.getPassword());
+                    user.setFirstName(request.getFirstName());
+                    user.setLastName(request.getLastName());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new AlreadyExistsException("Oops! "+request.getEmail()+" already exists!"));
+    }
+
+    @Override
+    public User updateUser(UpdateUserRequest request, Long userId) {
+        return userRepository.findById(userId).map(existingUser->{
+            existingUser.setFirstName(request.getFirstName());
+            existingUser.setLastName(request.getLastName());
+            return userRepository.save(existingUser);
+        }).orElseThrow(()->new ResourceNotFoundException("User Not Found!"));
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        userRepository.findById(userId).ifPresentOrElse(userRepository::delete,()->{
+            throw new ResourceNotFoundException("User Not Found");
+        });
+
+    }
+
+    @Override
+    public UserDto convertUserToDto(User user){
+        return modelMapper.map(user, UserDto.class);
+    }
 }
