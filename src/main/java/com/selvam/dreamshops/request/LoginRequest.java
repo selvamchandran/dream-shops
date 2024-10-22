a738d781abeae0e7a5b144ce5a8af6142db24d6a
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-22 19:04:50 +0530

    feat: login request created

--- src/main/java/com/selvam/dreamshops/request/LoginRequest.java
+++ src/main/java/com/selvam/dreamshops/request/LoginRequest.java
@@ -1,2 +1,15 @@
-package com.selvam.dreamshops.request;public class LoginRequest {
+package com.selvam.dreamshops.request;
+
+import jakarta.persistence.Access;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
 }
