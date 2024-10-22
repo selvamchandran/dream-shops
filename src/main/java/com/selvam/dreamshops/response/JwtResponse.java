a5809bc4709e0d5f6e7524da4b77918b8981065a
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-22 19:06:37 +0530

    feat: jwt response created

--- src/main/java/com/selvam/dreamshops/response/JwtResponse.java
+++ src/main/java/com/selvam/dreamshops/response/JwtResponse.java
@@ -1,2 +1,13 @@
-package com.selvam.dreamshops.response;public class JwtResponse {
+package com.selvam.dreamshops.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
+    private Long id;
+    private String token;
 }
