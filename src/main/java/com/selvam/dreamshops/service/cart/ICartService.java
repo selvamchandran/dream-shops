a5ed091d7d11ff36fa40be522d2ff5e4ceb8eddf
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-23 08:58:35 +0530

    feat: new cart initialization method added

--- src/main/java/com/selvam/dreamshops/service/cart/ICartService.java
+++ src/main/java/com/selvam/dreamshops/service/cart/ICartService.java
@@ -1,6 +1,7 @@
 package com.selvam.dreamshops.service.cart;
 
 import com.selvam.dreamshops.model.Cart;
+import com.selvam.dreamshops.model.User;
 
 import java.math.BigDecimal;
 
@@ -9,7 +10,7 @@ public interface ICartService {
     void clearCart(Long id);
     BigDecimal getTotalPrice(Long id);
 
-    Long initializeNewCart();
+    Cart initializeNewCart(User user);
 
     Cart getCartByUserId(Long userId);
 }
