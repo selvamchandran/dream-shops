eabe4a88f07fa8ea34db65e1e9835f9c5ec1bb4f
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-23 09:20:54 +0530

    fix: order date type changed

--- src/main/java/com/selvam/dreamshops/dto/OrderDto.java
+++ src/main/java/com/selvam/dreamshops/dto/OrderDto.java
@@ -5,12 +5,13 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 @Data
 public class OrderDto {
     private Long id;
     private Long userId;
-    private LocalDate OrderDate;
+    private LocalDateTime OrderDate;
     private BigDecimal totalAmount;
     private String status;
     private List<OrderItemDto> items;
