bdb05b3462c5ca78e60ed3f2e34e3c7ba33674a4
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-23 09:01:41 +0530

    feat: Shop user details using UserDetailsService added

--- src/main/java/com/selvam/dreamshops/security/user/ShopUserDetailsService.java
+++ src/main/java/com/selvam/dreamshops/security/user/ShopUserDetailsService.java
@@ -1,4 +1,26 @@
 package com.selvam.dreamshops.security.user;
 
-public class ShopUserDetails {
+import com.selvam.dreamshops.model.User;
+import com.selvam.dreamshops.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShopUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow(()->new UsernameNotFoundException("User Not Found!"));
+        return ShopUserDetails.buildUserDetails(user);
+    }
 }
