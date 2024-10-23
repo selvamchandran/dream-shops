f6d72ebd3296fdde3b5b730e78c40b07fa56564a
selvamchandran <selvam.v.chandran@gmail.com>
2024-10-23 10:31:16 +0530

    feat: Access Denied Exception added

--- src/main/java/com/selvam/dreamshops/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/selvam/dreamshops/exceptions/GlobalExceptionHandler.java
@@ -1,2 +1,19 @@
-package com.selvam.dreamshops.exceptions;public class GlobalExceptionHandler {
+package com.selvam.dreamshops.exceptions;
+
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.nio.file.AccessDeniedException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex)
+    {
+        String message = "You do not have permission to this action";
+        return new ResponseEntity<>(message, HttpStatus.FORBIDDEN);
+    }
 }
